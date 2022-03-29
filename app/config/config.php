cae071d2cc9238320801ff1e7df4ba8e7f0ab9f1
abderrahmane <abderrahamane7@gmail.com>
2022-03-29 15:47:20 +0000

    added database config varialbes and headers in the core file

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 // * db params
-define("DB_HOST", "");
-define("DB_USER", "");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "");
+define("DB_NAME", "appointment-reservation");
 
 // * approot
 define("APPROOT", dirname(dirname(__FILE__)));
